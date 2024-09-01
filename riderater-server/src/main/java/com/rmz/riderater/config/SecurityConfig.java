2ee49e11dcdaea55a8f96da4c98da2024d210c14
rzhang57 <48890532+rzhang57@users.noreply.github.com>
2024-09-01 11:25:00 -0700

    Oauth2.0 using google connected, permitAll() as of now
    endpoints not secured by authentication just yet for testing, but next step should be to connect authentication to frontend to be able to use the application. Also, only certain routes should be protected such as the ratings page.

--- riderater-server/src/main/java/com/rmz/riderater/config/SecurityConfig.java
+++ riderater-server/src/main/java/com/rmz/riderater/config/SecurityConfig.java
@@ -1,22 +1,25 @@
-//package com.rmz.riderater.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//public class SecurityConfig {
-//
-//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        return http
-//                .authorizeHttpRequests( auth -> {
-//                    auth.anyRequest().permitAll();
-//                })
-//                .oauth2Login(Customizer.withDefaults()) // oauth2 default providers using 3rd party apps (check app.prop for supported)
-//                .build();
-//    }
-//
-//
-//}
+package com.rmz.riderater.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests( auth -> {
+                    auth.anyRequest().permitAll();
+                })
+                .oauth2Login(Customizer.withDefaults()) // oauth2 default providers using 3rd party apps (check app.prop for supported)
+                .build();
+    }
+
+
+}
